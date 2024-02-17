6d2acb3a5f7b22062c9575943bdbf8c331bb1e6e
willanthoney <119621873+willanthoney@users.noreply.github.com>
2024-02-17 13:15:00 -0600

    Update Version1.java
    first test

--- app/src/main/java/compsci424/p1/java/Version1.java
+++ app/src/main/java/compsci424/p1/java/Version1.java
@@ -1,5 +1,5 @@
 /* COMPSCI 424 Program 1
- * Name:
+ * Name: Will Anthoney
  */
 package compsci424.p1.java;
 
@@ -13,24 +13,26 @@ package compsci424.p1.java;
  * classes, methods, and data structures that you need to solve the
  * problem and display your solution in the correct format.
  */
-public class Version1 {
-    // Declare any class/instance variables that you need here.
+import java.util.LinkedList;
 
+class Version1 implements ProcessManager{
+    // Declare any class/instance variables that you need here.
+	private Version1PCB[] pcbArray;
     /**
      * Default constructor. Use this to allocate (if needed) and
      * initialize the PCB array, create the PCB for process 0, and do
      * any other initialization that is needed. 
      */
-    public Version1() {
-
+    public Version1() {	
+    	pcbArray = new Version1PCB[16];
+        pcbArray[0] = new Version1PCB(-1);
     }
-
     /**
      * Creates a new child process of the process with ID parentPid. 
      * @param parentPid the PID of the new process's parent
      * @return 0 if successful, not 0 if unsuccessful
      */
-    int create(int parentPid) {
+    public int create(int parentPid) {
         // If parentPid is not in the process hierarchy, do nothing; 
         // your code may return an error code or message in this case,
         // but it should not halt
@@ -38,6 +40,13 @@ public class Version1 {
         // Assuming you've found the PCB for parentPid in the PCB array:
         // 1. Allocate and initialize a free PCB object from the array
         //    of PCB objects
+    	
+    	int newPid = findNextAvailablePid();
+        pcbArray[newPid] = new Version1PCB(parentPid);
+        if (pcbArray[parentPid].children == null) {
+            pcbArray[parentPid].children = new LinkedList<>();
+        }
+        pcbArray[parentPid].children.add(newPid);
 
         // 2. Insert the newly allocated PCB object into parentPid's
         //    list of children
@@ -46,14 +55,13 @@ public class Version1 {
         // If you change the return type/value(s), update the Javadoc.
         return 0; // often means "success" or "terminated normally"
     }
-
     /**
      * Recursively destroys the process with ID parentPid and all of
      * its descendant processes (child, grandchild, etc.).
      * @param targetPid the PID of the process to be destroyed
      * @return 0 if successful, not 0 if unsuccessful
      */
-    int destroy (int targetPid) {
+    public int destroy (int targetPid) {
          // If targetPid is not in the process hierarchy, do nothing; 
          // your code may return an error code or message in this case,
          // but it should not halt
@@ -67,12 +75,13 @@ public class Version1 {
 
          // 3. Deallocate targetPid's PCB and mark its PCB array entry
          //    as "free"
+    	
+    	destroyHelper(targetPid);
 
          // You can decide what the return value(s), if any, should be.
          // If you change the return type/value(s), update the Javadoc.
         return 0; // often means "success" or "terminated normally"
     }
-
     /**
      * Traverse the process creation hierarchy graph, printing
      * information about each process as you go. See Canvas for the
@@ -83,10 +92,41 @@ public class Version1 {
      * change the return type of this function to return the text to
      * the main program for printing. It's your choice. 
      */
-    void showProcessInfo() {
+    public void showProcessInfo() {
 
+    	for (int i = 0; i < pcbArray.length; i++) {
+            if (pcbArray[i] != null) {
+                System.out.print("Process " + i + ": parent is " + pcbArray[i].parent);
+                if (pcbArray[i].children != null && !pcbArray[i].children.isEmpty()) {
+                    System.out.print(" and children are ");
+                    for (int childPid : pcbArray[i].children) {
+                        System.out.print(childPid + " ");
+                    }
+                } else {
+                    System.out.print(" and has no children");
+                }
+                System.out.println();
+            }
+        }
+        System.out.println();
     }
-
     /* If you need or want more methods, feel free to add them. */
     
+    private void destroyHelper(int pid) {
+        if (pcbArray[pid].children != null) {
+            for (int childPid : pcbArray[pid].children) {
+                destroyHelper(childPid);
+            }
+        }
+        pcbArray[pid] = null;
+    }
+    
+    private int findNextAvailablePid() {
+        for (int i = 0; i < pcbArray.length; i++) {
+            if (pcbArray[i] == null) {
+                return i;
+            }
+        }
+        return -1;
+    }  
 }
