432aaaeccf84e06ade7de54ac28d21388cd9c111
willanthoney <119621873+willanthoney@users.noreply.github.com>
2024-02-21 18:50:02 -0600

    Update Program1.java

--- app/src/main/java/compsci424/p1/java/Program1.java
+++ app/src/main/java/compsci424/p1/java/Program1.java
@@ -1,156 +1,80 @@
 /* COMPSCI 424 Program 1
-
  * Name: Will Anthoney
- * 
- * This is a template. Program1.java *must* contain the main class
- * for this program. Otherwise, feel free to edit these files, even
- * these pre-written comments or my provided code. You can add any
- * classes, methods, and data structures that you need to solve the
- * problem and display your solution in the correct format.
  */
 package compsci424.p1.java;
 
-import java.util.Scanner;
 import java.util.LinkedList;
 import java.util.List;
-/**
- * Main class for this program. The required steps have been copied
- * into the main method as comments. Feel free to add more comments to
- * help you understand your code, or for any other reason. Also feel
- * free to edit this comment to be more helpful for you.
- */
+import java.util.Scanner;
+
 public class Program1 {
-    // Declare any class/instance variables that you need here.
-
-    /**
-     * @param args command-line arguments, which can be ignored
-     */
     public static void main(String[] args) {
-    	
-    	Scanner sc = new Scanner(System.in);
-    	List<String> commands = new LinkedList<>();
+        Scanner sc = new Scanner(System.in);
+        List<String> commands = new LinkedList<>();
 
-        // 1. Ask the user to enter commands of the form "create N",
-        //    "destroy N", or "end", where N is an integer between 0 
-        //    and 15.
-
-        // 2. While the user has not typed "end", continue accepting
-        //    commands. Add each command to a list of actions to take 
-        //    while you run the simulation.
-        // 3. When the user types "end" (or optionally any word that 
-        //    is not "create" or "destroy"), stop accepting commands 
-        //    and complete the following steps.
-    	
-    	System.out.println("Enter commands of the form 'create N', 'destroy N', or 'end' where N is an integer between 0 and 15:");
+        System.out.println("Enter commands (create N, destroy N, or end):");
         String input;
         while (!(input = sc.nextLine()).equals("end")) {
             commands.add(input);
         }
-        //
-        // Hint: Steps 2 and 3 refer to the same loop. ;-)
 
-        // 4. Create an object of the Version 1 class and an object of
-        //    the Version 2 class.
-        
-        Version1 version1 = new Version1();
-        Version2 version2 = new Version2();
+        Version1 version1 = new Version1(16);
+        Version2 version2 = new Version2(16);
 
-        // 5. Run the command sequence once with the Version 1 object, 
-        //    calling its showProcessTree method after each command to
-        //    show the changes in the tree after each command.
         System.out.println("Version 1:");
+        for (String i : commands) {
+            String[] parts = i.split(" ");
+            int pid = Integer.parseInt(parts[1]);
+            if (parts[0].equals("create")) {
+                version1.create(pid / 10, pid % 10);
+            } else if (parts[0].equals("destroy")) {
+                version1.destroy(pid);
+            }
+            version1.showProcessInfo();
+        }
+
+        System.out.println("Version 2:");
+        for (String i : commands) {
+            String[] parts = i.split(" ");
+            int pid = Integer.parseInt(parts[1]);
+            if (parts[0].equals("create")) {
+                version2.create(pid / 10, pid % 10);
+            } else if (parts[0].equals("destroy")) {
+                version2.destroy(pid);
+            }
+            version2.showProcessInfo();
+        }
+
         long startTimeV1 = System.currentTimeMillis();
-        runCommands(version1, commands, true);
+        for (int i = 0; i < 200; i++) {
+            for (String action : commands) {
+                String[] parts = action.split(" ");
+                int pid = Integer.parseInt(parts[1]);
+                if (parts[0].equals("create")) {
+                    version1.create(pid / 10, pid % 10);
+                } else if (parts[0].equals("destroy")) {
+                    version1.destroy(pid);
+                }
+            }
+        }
         long endTimeV1 = System.currentTimeMillis();
-        long runTimeV1 = endTimeV1 - startTimeV1;
-        System.out.println("Version 1 running time: " + runTimeV1 + " milliseconds");
+        long elapsedTimeV1 = endTimeV1 - startTimeV1;
+        System.out.println("Version 1 Running Time: " + elapsedTimeV1 + " milliseconds");
 
-        // 6. Repeat step 5, but with the Version 2 object.
-        System.out.println("\nVersion 2:");
         long startTimeV2 = System.currentTimeMillis();
-        runCommands(version2, commands, true);
+        for (int i = 0; i < 200; i++) {
+            for (String action : commands) {
+                String[] parts = action.split(" ");
+                int pid = Integer.parseInt(parts[1]);
+                if (parts[0].equals("create")) {
+                    version2.create(pid / 10, pid % 10);
+                } else if (parts[0].equals("destroy")) {
+                    version2.destroy(pid);
+                }
+            }
+        }
         long endTimeV2 = System.currentTimeMillis();
-        long runTimeV2 = endTimeV2 - startTimeV2;
-        System.out.println("Version 2 running time: " + runTimeV2 + " milliseconds");
-
-        // 7. Store the current system time in a variable
-
-        // ... then run the command sequence 200 times with Version 1.
-
-        // ... After this, store the new current system time in a
-        //     second variable. Subtract the start time from the end 
-        //     time to get the Version 1 running time, then display 
-        //     the Version 1 running time.
-        
-        long startTimeLoopV1 = System.currentTimeMillis();
-        for (int i = 0; i < 200; i++) {
-            runCommands(version1, commands, false);
-        }
-        long endTimeLoopV1 = System.currentTimeMillis();
-        long runTimeLoopV1 = endTimeLoopV1 - startTimeLoopV1;
-        System.out.println("\nVersion 1 running time (200 loops): " + runTimeLoopV1 + " milliseconds");
-
-        // 8. Repeat step 7, but with the Version 2 object.
-        
-        long startTimeLoopV2 = System.currentTimeMillis();
-        for (int i = 0; i < 200; i++) {
-            runCommands(version2, commands, false);
-        }
-        long endTimeLoopV2 = System.currentTimeMillis();
-        long runTimeLoopV2 = endTimeLoopV2 - startTimeLoopV2;
-        System.out.println("Version 2 running time (200 loops): " + runTimeLoopV2 + " milliseconds");     
+        long elapsedTimeV2 = endTimeV2 - startTimeV2;
+        System.out.println("Version 2 Running Time: " + elapsedTimeV2 + " milliseconds");
     }
-    
-    private static void runCommands(Object processManager, List<String> commands, boolean showInfo) {
-        if (processManager instanceof Version1) {
-            Version1 version1 = (Version1) processManager;
-            for (String command : commands) {
-                String[] parts = command.split(" ");
-                if (parts.length < 2) {
-                    continue;
-                }
-                String commandSplit = parts[0];
-                try {
-                    int pid = Integer.parseInt(parts[1]);
-                    if (commandSplit.equals("create")) {
-                        version1.create(pid);
-                        if (showInfo) {
-                            version1.showProcessInfo();
-                        }
-                    } else if (commandSplit.equals("destroy")) {
-                        version1.destroy(pid);
-                        if (showInfo) {
-                            version1.showProcessInfo();
-                        }
-                    }
-                } catch (NumberFormatException e) {
-                }
-            }
-        } else if (processManager instanceof Version2) {
-            Version2 version2 = (Version2) processManager;
-            for (String command : commands) {
-                String[] parts = command.split(" ");
-                if (parts.length < 2) {
-                    continue;
-                }
-                String commandSplit = parts[0];
-                try {
-                    int pid = Integer.parseInt(parts[1]);
-                    if (commandSplit.equals("create")) {
-                        version2.create(pid);
-                        if (showInfo) {
-                            version2.showProcessInfo();
-                        }
-                    } else if (commandSplit.equals("destroy")) {
-                        version2.destroy(pid);
-                        if (showInfo) {
-                            version2.showProcessInfo();
-                        }
-                    }
-                } catch (NumberFormatException e) {
-                }
-            }
-        }
-    }
-
 }
