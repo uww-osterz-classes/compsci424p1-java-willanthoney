af5feb45449ccf15fd513060cf34f1e3d3ba47ef
willanthoney <119621873+willanthoney@users.noreply.github.com>
2024-02-27 19:19:10 -0600

    Update Version1.java

--- app/src/main/java/compsci424/p1/java/Version1.java
+++ app/src/main/java/compsci424/p1/java/Version1.java
@@ -3,52 +3,66 @@
  */
 package compsci424.p1.java;
 
-import java.util.LinkedList;
+public class Version1 {
 
-class Version1 {
-    private Version1PCB[] pcbs;
-
+	private Version1PCB[] pcbArray;
+	//create array and initialize every value as free
     public Version1(int n) {
-        pcbs = new Version1PCB[n];
-        for (int i = 0; i < n; i++) {
-            pcbs[i] = new Version1PCB();
-        }
+    	pcbArray = new Version1PCB[n];
+    	for (int i = 0; i < n; i++) {
+    		pcbArray[i] = new Version1PCB(-1);
+    	}
     }
-
-    public void create(int parentPid, int pid) {
-        if (parentPid < 0 || parentPid >= pcbs.length) {
-            System.out.println("Parent process does not exist.");
-            return;
-        }
-        Version1PCB pcb = new Version1PCB();
-        pcb.setParent(parentPid);
-        pcbs[pid] = pcb;
-        pcbs[parentPid].addChild(pid);
+    // create method, checks if there's a free PCB
+    int create(int parentPid) {
+    	
+    	int childPid = findFreePCB();
+    	if (childPid != -1) {
+    		pcbArray[childPid].parent = parentPid;
+    		if (parentPid >= 0 && parentPid < pcbArray.length) {
+    			pcbArray[parentPid].children.add(childPid);
+    		}
+    		else {
+    			System.out.println("No free PCB slots.");
+    		}
+    	}
+        return 0;
     }
-
-    public void destroy(int targetPid) {
-        if (targetPid < 0 || targetPid >= pcbs.length) {
-            System.out.println("Process to destroy does not exist.");
-            return;
-        }
-        destroyHelper(targetPid);
+    // method to find free PCB within array
+    private int findFreePCB() {
+    	for (int i = 0; i < pcbArray.length; i++) {
+    		if(pcbArray[i].parent == -1) {
+    			return i;
+    		}
+    	}
+    	return -1; // no free PCB
     }
+    // destroy method
+    int destroy (int targetPid) {
 
-    private void destroyHelper(int targetPid) {
-        for (int child : pcbs[targetPid].getChildren()) {
-            destroyHelper(child);
-        }
-        pcbs[targetPid] = null;
+    	if(targetPid < 0 || targetPid >= pcbArray.length) {
+    		System.out.println("Invalid process ID.");
+    		return -1; // failed
+    	}
+    	destroyRecursively(targetPid);
+        return 0;
     }
-
-    public void showProcessInfo() {
-        for (int i = 0; i < pcbs.length; i++) {
-            System.out.print("Process " + i + ": parent is " + pcbs[i].getParent());
-            LinkedList<Integer> children = pcbs[i].getChildren();
-            if (!children.isEmpty()) {
+    // helper method for destroy method that recursively destroy child and then parent.
+    private void destroyRecursively(int targetPid) {
+    	for (int childPid : pcbArray[targetPid].children) {
+            destroyRecursively(childPid);
+        }
+    	pcbArray[targetPid].parent= -1;
+    	pcbArray[targetPid].children.clear();
+    }
+    // toString method to display results to user.
+    void showProcessInfo() {
+        for (int i = 0; i < pcbArray.length; i++) {
+            System.out.print("Process " + i + ": parent is " + pcbArray[i].parent);
+            if (!pcbArray[i].children.isEmpty()) {
                 System.out.print(" and children are ");
-                for (int child : children) {
-                    System.out.print(child + " ");
+                for (int childPid : pcbArray[i].children) {
+                    System.out.print(childPid + " ");
                 }
             } else {
                 System.out.print(" and has no children");
