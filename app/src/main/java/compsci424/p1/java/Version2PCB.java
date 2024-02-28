e29490b514f8f7e2663ae1d9cf55e77f17753c67
willanthoney <119621873+willanthoney@users.noreply.github.com>
2024-02-27 19:21:00 -0600

    Update Version2PCB.java

--- app/src/main/java/compsci424/p1/java/Version2PCB.java
+++ app/src/main/java/compsci424/p1/java/Version2PCB.java
@@ -3,48 +3,16 @@
  */
 package compsci424.p1.java;
 
-class Version2PCB {
-    private int parent;
-    private int firstChild;
-    private int youngerSibling;
-    private int olderSibling;
-
-    public Version2PCB() {
-        this.parent = -1;
-        this.firstChild = -1;
-        this.youngerSibling = -1;
-        this.olderSibling = -1;
-    }
-
-    public int getParent() {
-        return parent;
-    }
-
-    public void setParent(int parent) {
-        this.parent = parent;
-    }
-
-    public int getFirstChild() {
-        return firstChild;
-    }
-
-    public void setFirstChild(int firstChild) {
-        this.firstChild = firstChild;
-    }
-
-    public int getYoungerSibling() {
-        return youngerSibling;
-    }
-
-    public void setYoungerSibling(int youngerSibling) {
-        this.youngerSibling = youngerSibling;
-    }
-
-    public int getOlderSibling() {
-        return olderSibling;
-    }
-
-    public void setOlderSibling(int olderSibling) {
-        this.olderSibling = olderSibling;
-    }
+public class Version2PCB {
+	int parent;
+	int firstChild;
+	int youngerSibling;
+	int olderSibling;
+	
+	public Version2PCB(int parent) {
+		this.parent = parent;
+		this.firstChild = -1;
+		this.youngerSibling = -1;
+		this.olderSibling = -1;
+	}
 }
